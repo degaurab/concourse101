5dff854acd329178b519b36b23630a5af87ce2f1
Gaurab Dey <gdey@pivotal.io>
2015-12-15 15:09:04 -0800

    perks of replication

--- integration/apple-test.sh
+++ integration/apple-test.sh
@@ -4,6 +4,8 @@ source /etc/profile.d/chruby-with-default-ruby.sh
 
 cd fruits
 
+echo 'executing tests to check apple quality/quantity'
+
 ruby -r yaml -e"
   load_apple= YAML.load_file('apple.yml')
   if (load_apple['number']<200) || (load_apple['color'] != 'red')
